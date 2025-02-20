86db82e19982650588c49731338af661703ebfdf
FrontSolutions <augusto.piatto@gmail.com>
2025-02-20 09:06:51 -0300

    feat:

--- Laboratório de Programação 3/front/script.js
+++ Laboratório de Programação 3/front/script.js
@@ -1,31 +1,176 @@
-const button = document.getElementById('button')
+const listButton = document.getElementById("list-button");
+const createButton = document.getElementById("create-button");
+const editButton = document.getElementById("edit-button");
+const deleteButton = document.getElementById("delete-button");
 
-button.addEventListener('click', async function () {
-    let data = []
-    try {
-        button.disabled = true
+const listTab = document.getElementById("list-tab");
+const createTab = document.getElementById("create-tab");
+const editTab = document.getElementById("edit-tab");
+const deleteTab = document.getElementById("delete-tab");
 
-        const response = await fetch('http://localhost:8080/coffees')
-        data = await response.json()
-    } catch (error) {
-        console.error('Erro ao buscar dados:', error)
-        button.disabled = false
-    }
+const listContent = document.getElementById("list-content");
+const createBrand = document.getElementById("create-brand");
+const createFlavor = document.getElementById("create-flavor");
+const createValidity = document.getElementById("create-validity");
+const createSubmit = document.getElementById("create-submit");
 
-    const container = document.getElementById('container')
+const editSelect = document.getElementById("edit-select");
+const editBrand = document.getElementById("edit-brand");
+const editFlavor = document.getElementById("edit-flavor");
+const editValidity = document.getElementById("edit-validity");
+const editSubmit = document.getElementById("edit-submit");
 
-    data.forEach((coffee) => {
-        const coffeeItem = document.createElement('div')
-        coffeeItem.classList.add('coffee-item')
+const deleteSelect = document.getElementById("delete-select");
+const deleteSubmit = document.getElementById("delete-submit");
 
-        const idSpan = document.createElement('span')
-        idSpan.textContent = `ID: ${coffee.id}`
+let cookies = [];
 
-        const nameSpan = document.createElement('span')
-        nameSpan.textContent = coffee.name
+function closeAllTabs() {
+  listTab.classList.remove("active");
+  createTab.classList.remove("active");
+  editTab.classList.remove("active");
+  deleteTab.classList.remove("active");
+}
 
-        coffeeItem.appendChild(idSpan)
-        coffeeItem.appendChild(nameSpan)
-        container.appendChild(coffeeItem)
-    })
-})
+function disableCurrentButton(button) {
+  listButton.disabled = false;
+  createButton.disabled = false;
+  editButton.disabled = false;
+  deleteButton.disabled = false;
+  button.disabled = true;
+}
+
+async function listCookies() {
+  try {
+    const response = await fetch("http://localhost:8080/bolachas");
+    cookies = await response.json();
+    listContent.innerHTML = "";
+    cookies.forEach((cookie) => {
+      const item = document.createElement("div");
+      item.classList.add("bolacha-item");
+
+      const info = document.createElement("div");
+      info.classList.add("bolacha-info");
+      info.innerHTML = `
+      <strong>${cookie.marca}</strong>
+      <span>Sabor: ${cookie.sabor}</span>
+      <span>Validade: ${cookie.validade}</span>
+    `;
+
+      item.appendChild(info);
+      listContent.appendChild(item);
+    });
+  } catch (error) {
+    console.error("Erro ao buscar dados:", error);
+  }
+}
+
+listButton.addEventListener("click", async function () {
+  closeAllTabs();
+  listTab.classList.add("active");
+  disableCurrentButton(listButton);
+
+  await listCookies();
+});
+
+createButton.addEventListener("click", function () {
+  closeAllTabs();
+  createTab.classList.add("active");
+  disableCurrentButton(createButton);
+});
+
+createSubmit.addEventListener("click", async function () {
+  const newCookie = {
+    sabor: createBrand.value,
+    marca: createFlavor.value,
+    validade: createValidity.value,
+  };
+
+  try {
+    const response = await fetch("http://localhost:8080/bolachas", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newCookie),
+    });
+    const data = await response.json();
+    cookies.push(data);
+    createBrand.value = "";
+    createFlavor.value = "";
+    createValidity.value = "";
+    alert("Bolacha criada com sucesso!");
+  } catch (error) {
+    console.error("Erro ao criar bolacha:", error);
+  }
+});
+
+editButton.addEventListener("click", async function () {
+  closeAllTabs();
+  editTab.classList.add("active");
+  disableCurrentButton(editButton);
+  editSelect.innerHTML = "";
+  await listCookies();
+  cookies.forEach((cookie) => {
+    const option = document.createElement("option");
+    option.value = cookie.id;
+    option.textContent = `ID: ${cookie.id}, Marca: ${cookie.marca}`;
+    editSelect.appendChild(option);
+  });
+});
+
+editSubmit.addEventListener("click", async function () {
+  const selectedCookieId = editSelect.value;
+  const updatedCookie = {
+    marca: editBrand.value,
+    sabor: editFlavor.value,
+    validade: editValidity.value,
+  };
+
+  try {
+    const response = await fetch(
+      `http://localhost:8080/bolachas/${selectedCookieId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedCookie),
+      }
+    );
+    const data = await response.json();
+    const index = cookies.findIndex((cookie) => cookie.id === selectedCookieId);
+    cookies[index] = data;
+    alert("Bolacha editada com sucesso!");
+  } catch (error) {
+    console.error("Erro ao editar bolacha:", error);
+  }
+});
+
+deleteButton.addEventListener("click", async function () {
+  closeAllTabs();
+  deleteTab.classList.add("active");
+  disableCurrentButton(deleteButton);
+  deleteSelect.innerHTML = "";
+  await listCookies();
+  cookies.forEach((cookie) => {
+    const option = document.createElement("option");
+    option.value = cookie.id;
+    option.textContent = `ID: ${cookie.id}, Marca: ${cookie.marca}`;
+    deleteSelect.appendChild(option);
+  });
+});
+
+deleteSubmit.addEventListener("click", async function () {
+  const selectedCookieId = deleteSelect.value;
+
+  try {
+    await fetch(`http://localhost:8080/bolachas/${selectedCookieId}`, {
+      method: "DELETE",
+    });
+    cookies = cookies.filter((cookie) => cookie.id !== selectedCookieId);
+    alert("Bolacha deletada com sucesso!");
+  } catch (error) {
+    console.error("Erro ao deletar bolacha:", error);
+  }
+});
---
+++ Laboratório de Programação 3/java/sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
@@ -0,0 +1,159 @@
+package com.thehecklers.sburrestdemo;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
+
+@SpringBootApplication
+public class SburRestDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SburRestDemoApplication.class, args);
+  }
+
+}
+
+@RestController
+@RequestMapping("/bolachas")
+class RestApiDemoController {
+  private List<Bolacha> bolachas = new ArrayList<>();
+
+  public RestApiDemoController() {
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+    try {
+      bolachas.addAll(List.of(
+          new Bolacha("Trakinas", "baunilha", formatter.parse("13/10/2025")),
+          new Bolacha("Oreo", "chocolate", formatter.parse("12/10/2025")),
+          new Bolacha("Marina", "morango", formatter.parse("11/10/2025")),
+          new Bolacha("Tortinha", "limão", formatter.parse("10/10/2025"))));
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @CrossOrigin(origins = "*")
+  @GetMapping
+  Iterable<Bolacha> getBolachas() {
+    return bolachas;
+  }
+
+  @CrossOrigin(origins = "*")
+  @GetMapping("/{id}")
+  Optional<Bolacha> getBolachaById(@PathVariable String id) {
+    for (Bolacha b : bolachas) {
+      if (b.getId().equals(id)) {
+        return Optional.of(b);
+      }
+    }
+
+    return Optional.empty();
+  }
+
+  @CrossOrigin(origins = "*")
+  @PostMapping
+  Bolacha postBolacha(@RequestBody Bolacha bolacha) {
+    bolachas.add(bolacha);
+    return bolacha;
+  }
+
+  @CrossOrigin(origins = "*")
+  @PutMapping("/{id}")
+  ResponseEntity<Bolacha> putBolacha(@PathVariable String id,
+      @RequestBody Bolacha bolacha) {
+    int bolachaIndex = -1;
+
+    for (Bolacha b : bolachas) {
+      if (b.getId().equals(id)) {
+        bolachaIndex = bolachas.indexOf(b);
+        bolachas.set(bolachaIndex, bolacha);
+      }
+    }
+
+    return (bolachaIndex == -1) ? new ResponseEntity<>(postBolacha(bolacha), HttpStatus.CREATED)
+        : new ResponseEntity<>(bolacha, HttpStatus.OK);
+  }
+
+  @CrossOrigin(origins = "*")
+  @DeleteMapping("/{id}")
+  Bolacha deleteBolacha(@PathVariable String id) {
+    Bolacha bolacha = new Bolacha();
+
+    for (Bolacha b : bolachas) {
+      if (b.getId().equals(id)) {
+        bolacha = b;
+      }
+    }
+    bolachas.removeIf(b -> b.getId().equals(id));
+    return bolacha;
+  }
+}
+
+class Bolacha {
+  private final String id;
+  private String marca;
+  private String sabor;
+  private Date validade;
+
+  public Bolacha() {
+    this.id = UUID.randomUUID().toString();
+    this.marca = "Desconhecida";
+    this.sabor = "Desconhecido";
+    this.validade = new Date();
+  }
+
+  public Bolacha(String id, String marca, String sabor, Date validade) {
+    this.id = id;
+    this.marca = marca;
+    this.sabor = sabor;
+    this.validade = validade;
+  }
+
+  public Bolacha(String marca, String sabor, Date validade) {
+    this(UUID.randomUUID().toString(), marca, sabor, validade);
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getMarca() {
+    return marca;
+  }
+
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+  public String getSabor() {
+    return sabor;
+  }
+
+  public void setSabor(String sabor) {
+    this.sabor = sabor;
+  }
+
+  public Date getValidade() {
+    return validade;
+  }
+
+  public void setValidade(String validade) {
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+    try {
+      this.validade = formatter.parse(validade);
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+  }
+}
